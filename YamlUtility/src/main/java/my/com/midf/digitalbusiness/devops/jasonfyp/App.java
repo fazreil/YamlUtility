3d14f7f962d29475bfdf2d88683ddac2f0e23b40
Jason Wong <jason.wong@midf.com.my>
2023-01-03 16:32:54 +0800

    Initial draft

---
+++ YamlUtility/src/main/java/my/com/midf/digitalbusiness/devops/jasonfyp/App.java
@@ -0,0 +1,13 @@
+package my.com.midf.digitalbusiness.devops.jasonfyp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ YamlUtility/src/test/java/my/com/midf/digitalbusiness/devops/jasonfyp/AppTest.java
@@ -0,0 +1,20 @@
+package my.com.midf.digitalbusiness.devops.jasonfyp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
